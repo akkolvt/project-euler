6309e78a9f19a6798e93935457d10e32842bfe3c
akkolvt <shakhazizian@gmail.com>
2015-06-06 10:04:39 +0300

    Ruby solution for problem 3

---
+++ problem_3/problem_3.rb
@@ -0,0 +1,32 @@
+NUM = 600851475143
+
+def prime?(num)
+  return false if num.even?
+
+  n = Math.sqrt(num).ceil
+  div = nil
+
+  3.upto(n).select(&:odd?).each do |d|
+    if num % d == 0
+      div = d
+      break
+    end
+  end
+
+  div.nil?
+end
+
+n = 3
+divs = []
+left = NUM
+
+while n <= left do
+  if prime?(n) && left % n == 0
+    divs << n
+    left = left / n
+  end
+
+  n += 2
+end
+
+puts divs.last
